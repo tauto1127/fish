50a37db24868c418c815e2ba6f8691ddf821ee38
takuto1127 <tiharu717@gmail.com>
2024-10-19 15:04:34 +0900

    create home page and..

---
+++ lib/view/purpose_exist.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PurposeExist extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("目的地あり"),
+    );
+  }
+}
