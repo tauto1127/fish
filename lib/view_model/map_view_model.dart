740f7ef92bb3ed6e1a0fa85417e74c9c46e59227
takuto1127 <tiharu717@gmail.com>
2024-10-20 02:48:05 +0900

    色々直したり，ホーム画面つくた

--- lib/view_model/map_view_model.dart
+++ lib/view_model/map_view_model.dart
@@ -14,11 +14,29 @@ class MapViewModel extends _$MapViewModel {
   MapViewState build() {
     return const MapViewState(
       elements: [
-        Wall(point: Point(0, 4000), end: Point(6000, 4000)),
-        Floor(point: Point(0, 2000), width: 6000, height: 2000),
-        Room(point: Point(0, 0), width: 2000, height: 2000, room: RoomType.Computer363, door: Point(1000, 2000)),
-        Room(point: Point(2000, 0), width: 2000, height: 2000, room: RoomType.Computer364, door: Point(1000, 2000)),
-        Room(point: Point(4000, 0), width: 2000, height: 2000, room: RoomType.Computer365, door: Point(1000, 2000))
+        Wall(point: Point(0, 4000), end: Point(6000, 4000), floorNum: 3),
+        Floor(point: Point(0, 2000), width: 6000, height: 2000, floorNum: 3),
+        Room(
+            point: Point(0, 0),
+            width: 2000,
+            height: 2000,
+            room: RoomType.Computer363,
+            door: Point(1000, 2000),
+            floorNum: 3),
+        Room(
+            point: Point(2000, 0),
+            width: 2000,
+            height: 2000,
+            room: RoomType.Computer364,
+            door: Point(1000, 2000),
+            floorNum: 3),
+        Room(
+            point: Point(4000, 0),
+            width: 2000,
+            height: 2000,
+            room: RoomType.Computer365,
+            door: Point(1000, 2000),
+            floorNum: 3),
       ],
     );
   }
