54f7371457d16ec1bcb644f9e358cceae6d6184f
takuto1127 <tiharu717@gmail.com>
2024-10-20 00:34:09 +0900

    fix padding

--- lib/view/purpose_exist.dart
+++ lib/view/purpose_exist.dart
@@ -1,8 +1,6 @@
 import 'package:fish_hackathon/const/styles.dart';
 import 'package:fish_hackathon/main.dart';
-import 'package:fish_hackathon/model/map_element.dart';
 import 'package:fish_hackathon/view/navi_view.dart';
-import 'package:fish_hackathon/view_model/map_view_model.dart';
 import 'package:fish_hackathon/view_model/navi_view_model.dart';
 import 'package:fish_hackathon/view_model/purpose_exist_view_model.dart';
 import 'package:flutter/material.dart';
@@ -26,26 +24,40 @@ class PurposeExist extends StatelessWidget {
                     child: TextFormField(
                       decoration: MirasutaTextFormFieldDecoration,
                       onChanged: (value) {
-                        ref.read(purposeExistViewModelProvider.notifier).search(value);
+                        ref
+                            .read(purposeExistViewModelProvider.notifier)
+                            .search(value);
                       },
                     ),
                   ),
                   Expanded(
-                    child: ListView.builder(
-                      itemCount: state.length,
-                      itemBuilder: (context, index) {
-                        final element = state[index];
-                        return ListTile(
-                          title: Text(element.room.name),
-                          //subtitle: Text("(${element.x}, ${element.y})"),
-                          onTap: () {
-                            ref.read(naviViewModelProvider.notifier).setDestination(element.room);
-                            Navigator.of(context).push(MaterialPageRoute(builder: (context) {
-                              return const NaviView();
-                            }));
-                          },
-                        );
-                      },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: ListView.builder(
+                        itemCount: state.length,
+                        itemBuilder: (context, index) {
+                          final element = state[index];
+                          return Padding(
+                            padding: const EdgeInsets.only(bottom: 8),
+                            child: ListTile(
+                              tileColor: Colors.grey[300],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)),
+                              title: Text(element.room.name),
+                              //subtitle: Text("(${element.x}, ${element.y})"),
+                              onTap: () {
+                                ref
+                                    .read(naviViewModelProvider.notifier)
+                                    .setDestination(element.room);
+                                Navigator.of(context)
+                                    .push(MaterialPageRoute(builder: (context) {
+                                  return const NaviView();
+                                }));
+                              },
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
                 ],
